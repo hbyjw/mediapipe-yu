c00ff078e27e3c4eb57da79934529a3e25fbb1f8
hbyjw <hbyjw@hotmail.com>
2022-05-09 02:26:36 +0000

    1

--- build_knift_app.sh
+++ build_knift_app.sh
@@ -9,3 +9,5 @@ bazel build -c opt --config=android_arm64 mediapipe/examples/android/src/java/co
 # Switch back to OpenCV 3
 sed -i -e 's:4.0.1/opencv-4.0.1:3.4.3/opencv-3.4.3:g' WORKSPACE
 sed -i -e 's:libopencv_java4:libopencv_java3:g' third_party/opencv_android.BUILD
+
+cp bazel-bin/mediapipe/examples/android/src/java/com/google/mediapipe/apps/templatematchingcpu/templatematchingcpu.apk /home/github/mediapipe-yu/aar_dir
