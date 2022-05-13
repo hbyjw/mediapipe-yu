7696c89ede46dd0317c8da984a60d60eb6829f14
hbyjw <hbyjw@hotmail.com>
2022-05-13 06:26:49 +0000

    1

--- buildaar.sh
+++ buildaar.sh
@@ -6,6 +6,10 @@
 rm -rf bazel-bin/mediapipe/examples/android/src/java/com/google/mediapipe/apps/aar_example
 rm /home/github/mediapipe-yu/aar_dir/mediapipe_template_matching.aar  # && rm /home/github/mediapipe-yu/aar_dir/mediapipe_template_matching_android_lib.aar
 rm /home/github/mediapipe-yu/aar_dir/mobile_cpu.binarypb
+
+sed -i -e 's:3.4.3/opencv-3.4.3:4.0.1/opencv-4.0.1:g' WORKSPACE
+sed -i -e 's:libopencv_java3:libopencv_java4:g' third_party/opencv_android.BUILD
+
 bazel build -c opt --strip=ALWAYS     --host_crosstool_top=@bazel_tools//tools/cpp:toolchain     --fat_apk_cpu=arm64-v8a,armeabi-v7a     //mediapipe/examples/android/src/java/com/google/mediapipe/apps/aar_example:mediapipe_template_matching.aar
 
 
