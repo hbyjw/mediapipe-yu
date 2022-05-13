3f89f4124eb7ee4f8a8072c82a9383048925e967
hbyjw <hbyjw@hotmail.com>
2022-05-13 04:21:04 +0000

    1

--- buildaar.sh
+++ buildaar.sh
@@ -4,7 +4,7 @@
 #  LOG(INFO) << "TimedBoxListIdToLabelCalculator:Process " ;
 
 rm -rf bazel-bin/mediapipe/examples/android/src/java/com/google/mediapipe/apps/aar_example
-rm /home/github/mediapipe-yu/aar_dir/mediapipe_template_matching.aar && rm /home/github/mediapipe-yu/aar_dir/mediapipe_template_matching_android_lib.aar
+rm /home/github/mediapipe-yu/aar_dir/mediapipe_template_matching.aar  # && rm /home/github/mediapipe-yu/aar_dir/mediapipe_template_matching_android_lib.aar
 rm /home/github/mediapipe-yu/aar_dir/mobile_cpu.binarypb
 bazel build -c opt --strip=ALWAYS     --host_crosstool_top=@bazel_tools//tools/cpp:toolchain     --fat_apk_cpu=arm64-v8a,armeabi-v7a     //mediapipe/examples/android/src/java/com/google/mediapipe/apps/aar_example:mediapipe_template_matching.aar
 
@@ -13,7 +13,7 @@ bazel build -c opt --strip=ALWAYS     --host_crosstool_top=@bazel_tools//tools/c
 
 cp bazel-bin/mediapipe/examples/android/src/java/com/google/mediapipe/apps/aar_example/mediapipe_template_matching.aar /home/github/mediapipe-yu/aar_dir/
 
-cp bazel-bin/mediapipe/examples/android/src/java/com/google/mediapipe/apps/aar_example/mediapipe_template_matching_android_lib.aar /home/github/mediapipe-yu/aar_dir/
+# cp bazel-bin/mediapipe/examples/android/src/java/com/google/mediapipe/apps/aar_example/mediapipe_template_matching_android_lib.aar /home/github/mediapipe-yu/aar_dir/
 
 bazel build mediapipe/graphs/template_matching:mobile_cpu_binary_graph
 
